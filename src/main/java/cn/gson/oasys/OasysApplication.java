702997adfa1b9b4d1d08492a6f5f5bbf0b0df3af
810wangcheng <1041575391@qq.com>
2021-01-06 15:22:27 +0800

    first commit

---
+++ src/main/java/cn/gson/oasys/OasysApplication.java
@@ -0,0 +1,17 @@
+package cn.gson.oasys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+/**
+ * @author Administrator
+ */
+@SpringBootApplication
+public class OasysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OasysApplication.class, args);
+	}
+}
+
